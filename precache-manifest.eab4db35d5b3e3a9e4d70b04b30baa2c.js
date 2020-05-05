0d500862eb1749050a15b362bd2232f9efc0dbed
ATOM03 <priyanshujhaginia@outlook.com>
2020-05-05 23:21:13 +0530

    Updates

---
+++ precache-manifest.eab4db35d5b3e3a9e4d70b04b30baa2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090fcd3b8263a0f208974d4aae4b2e33",
+    "url": "/atom/index.html"
+  },
+  {
+    "revision": "17b19bbc57ea23dee2df",
+    "url": "/atom/static/css/main.167c2514.chunk.css"
+  },
+  {
+    "revision": "bfee2e9d681de460c3a7",
+    "url": "/atom/static/js/2.d4dc5261.chunk.js"
+  },
+  {
+    "revision": "fcdc5e536d87d4615bb78a0d5fa80be3",
+    "url": "/atom/static/js/2.d4dc5261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b19bbc57ea23dee2df",
+    "url": "/atom/static/js/main.8fb7cb08.chunk.js"
+  },
+  {
+    "revision": "e4d653b092d6adebdb71",
+    "url": "/atom/static/js/runtime-main.408d88e5.js"
+  }
+]);
